029c24a5d6ddc0ca320c39c79bd8a82890bb2ce5
FatimaALzahrani <12fatimah.15@gmail.com>
2023-03-14 15:49:46 +0300

    Finish part1 from SUBDOMAINS (string)

---
+++ JavaStringReverse.java
@@ -0,0 +1,15 @@
+//https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
+import java.io.*;
+import java.util.*;
+public class JavaStringReverse {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String re="";
+        for(int i=A.length()-1;i>=0;i--){
+            re+=A.charAt(i);
+        }
+        System.out.println((re.equals(A)?"Yes":"No"));
+    }
+}
---
+++ JavaStringTokens.java
@@ -0,0 +1,28 @@
+//https://www.hackerrank.com/challenges/java-string-tokens/problem?isFullScreen=true
+import java.io.*;
+import java.util.*;
+public class JavaStringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        ArrayList<String>list=new ArrayList<String>();
+        String re="";
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetter(s.charAt(i)))
+            re+=s.charAt(i);
+            else if(re.equals("")==false){
+            list.add(re);
+            re="";
+            }
+            if(re.equals("")==false && Character.isLetter(s.charAt(i)) && i==s.length()-1){
+            list.add(re);
+            }
+        }
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++)
+        System.out.println(list.get(i));
+        scan.close();
+    }
+}
+
